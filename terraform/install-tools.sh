825989156422c92cdfea54d0f878a13f87cadf62
AWSandAzureandFullStackEngineer <awsandazurecloudengineer@gmail.com>
2024-06-19 21:36:30 -0500

    apply infra.

--- terraform/install-tools.sh
+++ terraform/install-tools.sh
@@ -39,18 +39,7 @@ sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
 # Create EKS Fargate cluster
-eksctl create cluster \
-  --name=my-fargate-cluster \
-  --version=1.21 \
-  --region=us-west-2 \
-  --fargate \
-  --nodes=1 \
-  --nodes-min=1 \
-  --nodes-max=3 \
-  --auto-kubeconfig \
-  --alb-ingress-access \
-  --full-ecr-access
-
+eksctl create cluster --name=my-fargate-cluster --version=1.30 --region=us-east-1 --fargate --nodes=1 --nodes-min=1 --nodes-max=3 --auto-kubeconfig --alb-ingress-access --full-ecr-access
 # Wait for cluster to be ready
 kubectl wait --for=condition=Ready node --all --timeout=300s
 
